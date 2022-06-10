2cef99ee1cb496f0022403aa558908cafc35e18d
cabreradaniel14 <daniel_cabrera99@outlook.com>
2022-06-10 16:56:47 -0500

    IntroScreen

---
+++ lib/pages/IntroScreen/Introductionscreen.dart
@@ -0,0 +1,77 @@
+import 'package:agenda_app/pages/HomeScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class IntroductionScreen extends StatefulWidget {
+  const IntroductionScreen({Key? key}) : super(key: key);
+
+  @override
+  State<IntroductionScreen> createState() => _IntroductionScreenState();
+}
+
+class _IntroductionScreenState extends State<IntroductionScreen> {
+  PageController _controller = PageController();
+  bool onLastPage = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          PageView(
+            controller: _controller,
+            onPageChanged: (index) {
+              setState(() {
+                onLastPage = (index == 2);
+              });
+            },
+            children: [
+              Container(
+                color: Colors.green,
+              ),
+              Container(
+                color: Colors.red,
+              ),
+              Container(
+                color: Colors.yellow,
+              )
+            ],
+          ),
+          Container(
+            alignment: const Alignment(0, 0.75),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      _controller.jumpToPage(2);
+                    },
+                    child: const Text('Skip')),
+                SmoothPageIndicator(
+                    controller: _controller,
+                    effect: const WormEffect(),
+                    count: 3),
+                onLastPage
+                    ? GestureDetector(
+                        onTap: () {
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) {
+                            return const HomeScreen();
+                          }));
+                        },
+                        child: const Text('Done'))
+                    : GestureDetector(
+                        onTap: () {
+                          _controller.nextPage(
+                              duration: const Duration(milliseconds: 500),
+                              curve: Curves.easeIn);
+                        },
+                        child: const Text('Next'),
+                      )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/IntroScreen/introPage1.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class introPage1 extends StatelessWidget {
+  const introPage1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+      ),
+    );
+  }
+}
